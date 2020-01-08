5955821e478eea77c1f2f727dab7f53e5de75184
Ashton <10988492+ashco@users.noreply.github.com>
2020-01-07 19:56:54 -0800

    Integrating formik into project

--- src/app/components/Signin/SignInForm.tsx
+++ src/app/components/Signin/SignInForm.tsx
@@ -1,77 +1,120 @@
 ﻿import React, { useState, useContext } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  useField,
+  FormikProps,
+} from 'formik';
+import * as Yup from 'yup';
 
 import { FirebaseContext } from '../Firebase';
 import { IError } from '../Signup/SignUpForm';
 
-interface IState {
-  [key: string]: any;
+interface IFormValues {
   email: string;
   password: string;
-  error: null | IError;
+  // error: null | IError;
 }
 
-const INITIAL_STATE: IState = {
+const INITIAL_FORM_VALUES: IFormValues = {
   email: '',
   password: '',
-  error: null,
+  // error: null,
 };
 
 const SignInForm = () => {
   const firebase = useContext(FirebaseContext);
   const router = useRouter();
-  const [state, setState] = useState({ ...INITIAL_STATE });
-
-  const { email, password, error } = state;
-  const isInvalid = password === '' || email === '';
-
-  function handleChange(event: { target: { name: string; value: any } }): void {
-    const { name, value } = event.target;
-
-    const newState = { ...state };
-    newState[name] = value;
-
-    setState(newState);
-  }
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
-
-    firebase
-      .doSignInWithEmailAndPassword(email, password)
-      .then(() => {
-        setState({ ...INITIAL_STATE });
-        router.push('/');
-      })
-      .catch(error => {
-        setState({ ...state, error });
-      });
-  }
 
   return (
-    <FormWrapper onSubmit={handleSubmit}>
-      <input
-        name="email"
-        value={email}
-        onChange={handleChange}
-        type="text"
-        placeholder="Email Address"
-      />
-      <input
-        name="password"
-        value={password}
-        onChange={handleChange}
-        type="password"
-        placeholder="Password"
-      />
-      <button disabled={isInvalid} type="submit">
-        Sign In
-      </button>
-      {error && <p>{error.message}</p>}
-    </FormWrapper>
+    <Formik
+      initialValues={INITIAL_FORM_VALUES}
+      validationSchema={Yup.object({
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Required'),
+      })}
+      onSubmit={({ email, password }, { resetForm }) => {
+        firebase
+          .doSignInWithEmailAndPassword(email, password)
+          .then(() => {
+            resetForm();
+            router.push('/');
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }}
+    >
+      <Form>
+        {/* <TextInput
+          label="Email"
+          name="email"
+          type="email"
+          placeholder="janedoe@gmail.com"
+        /> */}
+        <label htmlFor="email">Password</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <label htmlFor="password">Password</label>
+        <Field name="password" type="password" />
+        <ErrorMessage name="password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
+
+// // Shape of form values
+// interface FormValues {
+//   email: string;
+//   password: string;
+// }
+
+// interface OtherProps {
+//   message: string;
+// }
+
+// const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
+//   const { touched, errors, isSubmitting, message } = props;
+//   return (
+//     <Form>
+//       <h1>{message}</h1>
+//       <Field type="email" name="email" />
+//       {touched.email && errors.email && <div>{errors.email}</div>}
+
+//       <Field type="password" name="password" />
+//       {touched.password && errors.password && <div>{errors.password}</div>}
+
+//       <button type="submit" disabled={isSubmitting}>
+//         Submit
+//       </button>
+//     </Form>
+//   );
+// };
+
+// interface FormValues = {
+
+// }
+
+// const TextInput = (props: FormikProps<FormValues>) => {
+//   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+//   // which we can spread on <input> and also replace ErrorMessage entirely.
+//   const [field, meta] = useField(props);
+//   return (
+//     <>
+//       <label htmlFor={props.id || props.name}>{label}</label>
+//       <input {...field} {...props} />
+//       {meta.touched && meta.error ? (
+//         <div className="error">{meta.error}</div>
+//       ) : null}
+//     </>
+//   );
+// };
 
 const FormWrapper = styled.form`
   /* border: black 4px solid; */
