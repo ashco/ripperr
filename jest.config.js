51a34b75f0609a746e4b3435335233fc0fee810f
Ashton <10988492+ashco@users.noreply.github.com>
2020-05-12 14:25:50 -0700

    Finalized tooling setup. Script are good to go. Husky + lint-staged will now run jest on related tests only. Should make commits faster

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
-const { compilerOptions } = require('./src/app/tsconfig.json');
+// const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// // which contains the path mapping (ie the `compilerOptions.paths` option):
+// const { compilerOptions } = require('./src/app/tsconfig.json');
 const path = require('path');
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  // preset: 'ts-jest',
+  // testEnvironment: 'jest-environment-jsdom',
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   moduleDirectories: ['node_modules', path.join(__dirname, 'src/app')],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   // snapshotSerializers: ['jest-styled-components'], // This packaged needs to be fixed to work with styled-components 7.0
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.jest.json',
-    },
-  },
+  // globals: {
+  //   'ts-jest': {
+  //     tsConfig: 'tsconfig.jest.json',
+  //   },
+  // },
   collectCoverageFrom: ['**/src/app/**/*.(js|jsx|ts|tsx)'],
   coverageThreshold: {
     global: {
@@ -25,4 +25,10 @@ module.exports = {
       lines: 1,
     },
   },
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  projects: ['./test/jest.lint.js', './test/jest.app.js'],
 };
---
+++ test/jest.app.js
@@ -0,0 +1,19 @@
+﻿const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('../src/app/tsconfig.json');
+
+module.exports = {
+  ...require('../jest.config'),
+  displayName: 'app',
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  // snapshotSerializers: ['jest-emotion'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  globals: {
+    'ts-jest': {
+      tsConfig: './test/tsconfig.jest.json',
+    },
+  },
+};
