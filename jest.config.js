e91404eebff88c8aac6a977da2875054977c5ddf
Ashton <10988492+ashco@users.noreply.github.com>
2020-05-12 12:50:55 -0700

    Added in travis config file. Set up testing watch and coverage scripts

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,19 @@ module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   moduleDirectories: ['node_modules', path.join(__dirname, 'src/app')],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  // snapshotSerializers: ['jest-styled-components'],
+  // snapshotSerializers: ['jest-styled-components'], // This packaged needs to be fixed to work with styled-components 7.0
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.jest.json',
+    },
+  },
+  collectCoverageFrom: ['**/src/app/**/*.(js|jsx|ts|tsx)'],
+  coverageThreshold: {
+    global: {
+      statements: 1,
+      branches: 0,
+      functions: 1,
+      lines: 1,
     },
   },
 };
